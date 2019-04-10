8d3e96a1d6c0bb4a678cf7940d0f1b96c087fbac
Muhammad Haekal <93haekal@gmail.com>
2019-04-10 14:30:40 +0700

    Finish News List Mockup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MainContainer>
+        <FilterBox>1</FilterBox>
+        <ListNewsContainer>
+          <NewsThumbnail>
+            <NewsDetail>
+              <NewsTitle>Mobil Masuk Jurang</NewsTitle>
+              <span>
+                <b>Author :</b> Joni <b>Source :</b> Kumparan <span />
+              </span>
+              <NewsThumbParagraph>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Architecto ex accusamus sequi, voluptatum distinctio rem
+                cupiditate consequatur quae totam ad non at id. Pariatur tempora
+                odio nemo adipisci, sunt laborum. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Ab, molestiae. Voluptatibus,
+                voluptatem dicta nostrum, nihil hic fuga ratione voluptates et
+                perspiciatis nam assumenda! Commodi obcaecati quam soluta
+                adipisci iusto mollitia! Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Enim nam nobis voluptatum minima at, facere
+                rerum culpa distinctio similique vitae consequatur impedit ipsa
+                autem iste fuga deserunt quos dolor maxime.
+                <FadeEffect />
+              </NewsThumbParagraph>
+            </NewsDetail>
+            <ImgThumbnail />
+          </NewsThumbnail>
+        </ListNewsContainer>
+      </MainContainer>
     );
   }
 }
+
+const MainContainer = styled.div({
+  display: "grid",
+  gridTemplateColumns: "15% 70% 15%"
+});
+
+const ListNewsContainer = styled.div({});
+
+const FilterBox = styled.div({});
+
+const NewsThumbnail = styled.div({
+  padding: "15px",
+  border: "2px solid grey",
+  borderRadius: "5px",
+  display: "grid",
+  gridTemplateColumns: "70% 30%",
+  marginBottom: "15px"
+});
+
+const NewsDetail = styled.div({
+  padding: "15px",
+  height: "200px",
+  boxSizing: "border-box"
+});
+
+const ImgThumbnail = styled.div({
+  backgroundImage:
+    "url(https://thumbor.forbes.com/thumbor/600x315/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1140163617%2F960x0.jpg%3Ffit%3Dscale)",
+  backgroundSize: "cover",
+  borderRadius: "5px",
+  height: "200px"
+});
+
+const NewsTitle = styled.h2({
+  marginTop: "0"
+});
+
+const NewsThumbParagraph = styled.p({
+  maxHeight: "100px",
+  overflow: "hidden",
+  textAlign: "justify",
+  position: "relative"
+});
+
+const FadeEffect = styled.div({
+  position: "absolute",
+  bottom: "0px",
+  display: "block",
+  width: "100%",
+  height: "50px",
+  backgroundImage:
+    "linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.9) 100%) "
+});
 
 export default App;
